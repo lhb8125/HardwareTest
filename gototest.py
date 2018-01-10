2cb0962cabcf206f4b972db4ea6dd86e9bf0b040
lhb8125 <lhb8125@users.noreply.github.com>
2018-01-10 18:58:14 +0800

    Add files via upload

--- gototest.py
+++ gototest.py
@@ -65,28 +65,26 @@ def concatelist(sample):
     return res
 
 def prettyprint():
-    fout = open("out.csv", 'w')
+    fout = open("out.txt", 'w')
 
 #-------------------------------OUTPUT BASIC-------------------------------#
     fout.write("operation release information," + op_release_info + "\n")
     fout.write("cpu physical number," + str(cpu_number) + "\n")
     fout.write("gpu device number," + str(device_number) + "\n")
-    fout.write("\n")
 #-------------------------------OUTPUT BASIC END-------------------------------#
 
 #-------------------------------OUTPUT CPU-------------------------------#
-    fout.write("CPU\n")
+    fout.write("cpu\n")
     cpu_model = "CPU name,"+cpuinfo_list[2]+'\n'
     cpu_core = "CPU core(s),"+cpuinfo_list[1]+'\n'
     cpu_threadpercore = "CPU thread per core,"+cpuinfo_list[0]+'\n'
     fout.write(cpu_model)
     fout.write(cpu_core)
     fout.write(cpu_threadpercore)
-    fout.write("\n")
 #-------------------------------OUTPUT CPU END-------------------------------#
 
 #-------------------------------OUTPUT GPU-------------------------------#
-    fout.write("GPU\n")
+    fout.write("gpu\n")
     fout.write("gpu,")
     for cnt in range(device_number-1):
         fout.write("device" + str(cnt) + ",")
@@ -103,15 +101,15 @@ def prettyprint():
     fout.write(gpu_core)
     gpu_clock = "main clock," + concatelist(gpu_mainclock)
     fout.write(gpu_clock)
-    gpu_mem_bus = "Bus width," + concatelist(memory_bus_w)
+    gpu_mem_bus = "memory bus width," + concatelist(memory_bus_w)
     fout.write(gpu_mem_bus)
-    gpu_single_flops = "Single float(GFLOPS)," + concatelist(single_f)
+    gpu_single_flops = "GFLOPS(single float)," + concatelist(single_f)
     fout.write(gpu_single_flops)
-    gpu_d2d = "Memory bandwidth(MB/s)," + concatelist(d2d)
+    gpu_d2d = "gpu memory bandwidth(MB/s)," + concatelist(d2d)
     fout.write(gpu_d2d)
-    gpu_h2d = "CPU to GPU(MB/s)," + concatelist(h2d)
+    gpu_h2d = "gpu host2device bandwidth(MB/s)," + concatelist(h2d)
     fout.write(gpu_h2d)
-    gpu_d2h = "GPU to CPU(MB/s)," + concatelist(d2h)
+    gpu_d2h = "gpu device2host bandwidth(MB/s)," + concatelist(d2h)
     fout.write(gpu_d2h)
 #-------------------------------OUTPUT GPU END-------------------------------#
 
--- test-one-click.sh
+++ test-one-click.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-export PATH=${PATH}:/usr/local/cuda/bin
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/cuda/lib64
-
 CUDASAMPLES="/home/tusimple/HardwareTest/cudaSamples"
 FILENAME="$CUDASAMPLES/7_CUDALibraries/batchCUBLAS/batchCUBLAS.cpp"
 
@@ -13,9 +10,9 @@ sed -i 's/^#define BENCH_MATRIX_N.*/#define BENCH_MATRIX_N  (4096)/g' $FILENAME
 DEVICE_QUERY="$CUDASAMPLES/1_Utilities/deviceQuery/"
 BANDWIDTH_TEST="$CUDASAMPLES/1_Utilities/bandwidthTest/"
 FLOPS_TEST="$CUDASAMPLES/7_CUDALibraries/batchCUBLAS/"
-cd $DEVICE_QUERY;make clean;make
-cd $BANDWIDTH_TEST;make clean;make
-cd $FLOPS_TEST;make clean;make
+cd $DEVICE_QUERY;make
+cd $BANDWIDTH_TEST;make
+cd $FLOPS_TEST;make
 
 cd /home/tusimple/HardwareTest
 python gototest.py
