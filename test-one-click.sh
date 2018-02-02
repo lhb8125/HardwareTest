6e39b1b1df07106f493d3197c781aef396439795
lhb8125 <lhb8125@users.noreply.github.com>
2018-02-02 11:36:25 +0800

    增加环境变量

--- test-one-click.sh
+++ test-one-click.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export PATH=/usr/local/cuda/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+
 CUDASAMPLES="/home/tusimple/HardwareTest/cudaSamples"
 FILENAME="$CUDASAMPLES/7_CUDALibraries/batchCUBLAS/batchCUBLAS.cpp"
 
